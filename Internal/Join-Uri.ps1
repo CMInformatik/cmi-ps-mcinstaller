21f9839d7335c258d75532e8611dea473cb0208d
Elia Seikritt <git@seikritt.ch>
2018-11-20 10:28:10 +0100

    Remove not used functions

--- Internal/Join-Uri.ps1
+++
@@ -1,8 +0,0 @@
-﻿function Join-Uri {
-    param([System.Uri]$Uri, [string]$RelativeUri)
-    $result = $null
-    if(-not [System.Uri]::TryCreate($Uri, $RelativeUri, [ref]$result)){
-        throw "Can not join $($Uri.ToString()) and $RelativeUri to a valid uri"
-    }
-    return ([System.Uri]$result)
-}

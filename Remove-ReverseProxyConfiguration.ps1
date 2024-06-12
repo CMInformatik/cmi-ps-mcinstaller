1202606edd12f875f0ad9149b4b48ef75dd80cc0
Fabio <113338073+Fabio-Ciarlesi@users.noreply.github.com>
2024-06-12 13:04:18 +0200

    Ops24 465 (#20)
    * Create Add-ReverseProxyConfiguration.ps1
    
    * OPS24-465
    
    Version 0.6
    Add/Remove/Update-ReverseProxyConfiguration
    
    * OPS24-465
    
    Fix Kommentare

---
+++ Remove-ReverseProxyConfiguration.ps1
@@ -0,0 +1,59 @@
+﻿function Remove-ReverseProxyConfiguration {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')] # no system changes
+    [CmdletBinding(SupportsShouldProcess = $false, ConfirmImpact = 'None')]
+    [OutputType([string])]
+    PARAM(
+        [parameter(Mandatory = $True, Position = 0, ValueFromPipelineByPropertyName = $True, ValueFromPipeline = $true )]
+        [ValidateNotNull()]
+        [JsonConfiguration]$Configuration,
+
+        [parameter(Mandatory = $True, Position = 1, ValueFromPipelineByPropertyName = $True)]
+        [ValidateScript({ MustBeValidTenantName $_ })]
+        [Alias('Name')]
+        [String[]]$TenantName,
+
+        [parameter(Mandatory = $True, Position = 2, ValueFromPipelineByPropertyName = $True)]
+        [String]$ProxyConfigurationPath 
+    )   
+    BEGIN {
+        function Remove-TenantUrls {
+            param (
+                $proxyConf,
+                $tenantName
+            )
+
+            $propertiesToRemove = @(
+                "$tenantName`pri",
+                "$tenantName`pub",
+                "$tenantName`db",
+                "$tenantName`sv",
+                "$tenantName`zd"
+            )
+
+            foreach ($property in $propertiesToRemove) {
+                if ($proxyConf.PSObject.Properties.Match($property).Count -gt 0) {
+                    $proxyConf.PSObject.Properties.Remove($property)
+                }
+            }
+        }
+    }
+    PROCESS {
+        if (Test-Path -Path $ProxyConfigurationPath) {
+            $proxyConf = Get-Content -Path $ProxyConfigurationPath -Raw | ConvertFrom-Json
+        } else {
+            throw "The file at path $ProxyConfigurationPath does not exist."
+        }
+
+        if ($null -eq $proxyConf) {
+            throw "Failed to read configuration or configuration is empty."
+        }
+
+        foreach ($tenant in $Configuration.Tenants) {
+            if ($tenant.Name -in $TenantName) {
+                Remove-TenantUrls -proxyConf $proxyConf -tenantName $tenant.Name
+            }
+        }
+
+        $proxyConf | ConvertTo-Json -Depth 99 | Out-File -FilePath $ProxyConfigurationPath -Force
+    }
+}
---
+++ Update-ReverseProxyConfiguration.ps1
@@ -0,0 +1,32 @@
+﻿function Update-ReverseProxyConfiguration {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')] # no system changes
+    [CmdletBinding(SupportsShouldProcess = $false, ConfirmImpact = 'None')]
+    [OutputType([string])]
+    PARAM(
+        [parameter(Mandatory = $True, Position = 0, ValueFromPipelineByPropertyName = $True, ValueFromPipeline = $true )]
+        [ValidateNotNull()]
+        [JsonConfiguration]$Configuration,
+
+        [parameter(Mandatory = $True, Position = 1, ValueFromPipelineByPropertyName = $True)]
+        [ValidateScript({ MustBeValidTenantName $_ })]
+        [Alias('Name')]
+        [String[]]$TenantName,
+
+        [parameter(Mandatory = $True, Position = 3, ValueFromPipelineByPropertyName = $True)]
+        [String]$ProxyConfigurationPath,
+
+        [parameter(Mandatory = $false, Position = 2, ValueFromPipelineByPropertyName = $True )]
+        [ValidateNotNull()]
+        [uri]$RelayServer = [uri]"https://relay.cmiaxioma.ch"
+
+    )   
+    BEGIN {
+        
+    }
+    PROCESS {
+        $Configuration | Remove-ReverseProxyConfiguration -TenantName $TenantName -ProxyConfigurationPath $ProxyConfigurationPath
+        $Configuration|Add-ReverseProxyConfiguration -TenantName $TenantName -RelayServer $RelayServer -ProxyConfigurationPath $ProxyConfigurationPath
+
+    }
+
+}
